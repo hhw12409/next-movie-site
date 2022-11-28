f9bc3bae18549669968bf97e8d462cdb27ba6a3d
hhw12409 <sksk2425@naver.com>
2022-11-29 02:37:58 +0900

    Feat: movies detail page rewrites setting

--- next.config.js
+++ next.config.js
@@ -29,6 +29,10 @@ const nextConfig = {
         source: "/api/movies",
         destination: `${BASE_URL}/movie/popular?api_key=${API_KEY}`,
       },
+      {
+        source: "/api/movies/:id",
+        destination: `${BASE_URL}/movie/:id?api_key=${API_KEY}`,
+      },
     ];
   },
 };
